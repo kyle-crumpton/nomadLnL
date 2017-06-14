342aacbd177d7f78812760deeb99b9309c405a66
Kyle Crumpton <kyle.crumpton@q2ebanking.com>
2017-06-14 13:51:44 -0500

    Move var for version out, upgrade dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM golang:1.8
 
+ENV VERSION "0.0.1"
+
 WORKDIR /go/src/app
 COPY . /go/src/app
 
 RUN go get && \
-    go build
+    go build -ldflags "-X main.version=0.0.1" 
 
 EXPOSE 8080
 
--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-
 	router := NewRouter()
-
+	fmt.Printf("Welcome to the Nomad LNL App!\n")
+	fmt.Printf("Version: " + version + "\n")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
